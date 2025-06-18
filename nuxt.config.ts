344e01f43c401db000567de17c55ff1d2a29af27
GabrielLacerda000 <gabrielglacerda000@gmail.com>
2025-06-18 17:27:00 -0300

    feat: libs and config added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/icon',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxt/fonts'
+  ],
+  app: {
+    head: {
+      bodyAttrs: {
+        class: 'bg-black text-white font-semibold font-mono'
+      }
+    },
+    pageTransition: { name: 'page', mode: 'out-in' }
+  }
 })
