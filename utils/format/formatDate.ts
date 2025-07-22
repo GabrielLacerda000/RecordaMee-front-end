948e9956441c80837634ef9f511d9cbaf5120f54
GabrielLacerda000 <gabrielglacerda000@gmail.com>
2025-07-22 18:35:53 -0300

    fix: timezone bug fixed

--- utils/format/formatDate.ts
+++ utils/format/formatDate.ts
@@ -2,7 +2,8 @@ const formatDate = (dateString: string) => {
   const options: Intl.DateTimeFormatOptions = { 
     day: '2-digit',
     month: '2-digit',
-    year: 'numeric'     
+    year: 'numeric',
+    timeZone: 'UTC',
    };
   return new Date(dateString).toLocaleDateString('pt-BR', options);
 };
