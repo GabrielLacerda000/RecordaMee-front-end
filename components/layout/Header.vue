09d1abcf2f4438e7f01f1913df0140846edf44f4
GabrielLacerda000 <gabrielglacerda000@gmail.com>
2025-07-23 09:51:52 -0300

    feat: total overdue added

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -53,6 +53,16 @@ const formatCurrency = (value: number) => {
           <Icon name="material-symbols:calendar-today-outline" class="text-yellow-600" size="2em" />
         </div>
       </div>
+
+      <div class="flex justify-between items-center p-3 rounded-lg bg-[#222526] shadow-md min-w-[250px]">
+        <div class="flex flex-col text-white">
+          <p class="text-md">Total Vencido:</p>
+          <span class="text-xl font-bold">{{ formatCurrency(summary?.totalOverdue || 0) }}</span>
+        </div>
+        <div class="w-12 h-12 rounded-full bg-red-200 flex items-center justify-center">
+          <Icon name="tabler:calendar-clock" class="text-red-600" size="2em" />
+        </div>
+      </div>
     </div>
     <!-- <InputsSearchInput /> -->
   </header>
--- types/expense/Sumary.ts
+++ types/expense/Sumary.ts
@@ -2,4 +2,5 @@ export type Summary = {
     total: number;
     totalPaid: number;
     totalPending: number;
+    totalOverdue: number;
 }
